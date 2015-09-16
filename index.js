7b8fdfe36de30764f350442e41aefc74eed691aa
Umayr Shahid <umayrr@hotmail.co.uk>
2015-09-16 17:41:06 +0500

    0.1.0

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Umayr Shahid on 9/16/2015.
+ */
+
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+  module.exports = 'cressida';
+}
+
+(function (window, angular) {
+  'use strict';
+
+  // Polyfill for array.include
+  Array.prototype.includes = function (value) {
+    return this.indexOf(value) > -1;
+  }
+
+  var cressida = require('cressida');
+
+  module.exports = angular
+    .module('cressida', [])
+    .factory('$cressida', function () {
+      var instance = null;
+
+      function create(options) {
+        instance = cressida.create(options);
+        return instance;
+      }
+
+      function message() {
+        return instance === null ? create() : instance;
+      }
+
+      return {
+        create: create,
+        message: message
+      };
+    });
+
+}(window, window.angular));
